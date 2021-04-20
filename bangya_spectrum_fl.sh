9cc60bd0da1228b3297ae72f37e3af6b82260bd7
Bangya Liu <liubangya@gmail.com>
2021-04-20 18:06:36 -0500

    stage 1 accomplished

--- bangya_spectrum_fl.sh
+++ bangya_spectrum_fl.sh
@@ -35,6 +35,9 @@ python3 ./spectrum_fedavg.py \
 --client_optimizer $OPT \
 --lr $LR 
 
+echo "loss should mulitiply with 55.27 to retrieve a dB^2 unit"
+echo "all training done! heatmap can be checked at fedml_experiments/standalone/spectrum_avg/"
+
 cd ./../../../
 
 
